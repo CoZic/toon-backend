60cd3f5dce9d56a1059542915ce892998adf04ad
shpark <maru4737@naver.com>
2025-06-09 13:52:39 +0900

    20250609 소스 커밋

---
+++ src/test/java/com/be/prac_toon/PracToonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.prac_toon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracToonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
