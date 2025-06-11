258abce9d7c75c50065c3007b12acb7b46c1d341
shpark <maru4737@naver.com>
2025-06-11 16:04:06 +0900

    user 조회 test

--- src/main/java/com/be/prac_toon/PracToonApplication.java
+++ src/main/java/com/be/prac_toon/PracToonApplication.java
@@ -12,11 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class PracToonApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello HelloHelloHelloHelloHello";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(PracToonApplication.class, args);
 	}
