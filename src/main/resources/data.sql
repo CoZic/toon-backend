6014ed8de156ceecf2549c66e3fc966ba0b18dd0
KBroJ <kbroj9210@gmail.com>
2025-06-17 23:11:35 +0900

    메인페이지 DB 데이터 쿼리문

---
+++ src/main/resources/data.sql
@@ -0,0 +1,33 @@
+-- 데이터가 이미 존재할 경우 중복 삽입을 방지하려면 아래 라인을 활성화하세요.
+--CREATE TABLE webtoon (
+--    id bigint not null auto_increment,
+--    author varchar(255),
+--    category varchar(255),
+--    thumbnail_url varchar(255),
+--    title varchar(255),
+--    primary key (id)
+--)
+-- DROP TABLE webtoon;
+-- DELETE FROM webtoon;
+
+
+-- INSERT IGNORE : Primary Key가 중복될 경우, 에러를 발생시키지 않고 조용히 해당 INSERT를 무시합니다.
+-- ★★★ MySQL/MariaDB에서만 사용 가능
+
+-- 'today' 카테고리 데이터
+INSERT IGNORE INTO webtoon (title, author, thumbnail_url, category) VALUES
+('나 혼자만 레벨업', '추공', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+1', 'today'),
+('전지적 독자 시점', '싱숑', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+2', 'today'),
+('화산귀환', '비가', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+3', 'today'),
+('세이렌', '설레다', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+4', 'today'),
+('입학용병', 'YC', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+5', 'today')
+;
+
+-- 'popular' 카테고리 데이터
+INSERT IGNORE INTO webtoon (title, author, thumbnail_url, category) VALUES
+('알고있지만', '정서', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+8', 'popular'),
+('유미의 세포들', '이동건', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+7', 'popular'),
+('신의 탑', 'SIU', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+10', 'popular'),
+('외모지상주의', '박태준', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+9', 'popular'),
+('더 복서', '정지훈', 'https://via.placeholder.com/200x250.png?text=DB+Webtoon+11', 'popular')
+;
